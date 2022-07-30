6f181aac2d094035f56e4a229ee543221a234ae9
Vishakha Manapure <vishakhamanapure23@gmail.com>
2022-07-30 15:33:07 +0530

    new file

---
+++ src/main/java/com/example/demo/JenkinsApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApp1Application.class, args);
+	}
+
+}
